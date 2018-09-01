01554be7619a7c7993af5b8760d3846c2fec25e0
Cody Reed <cody2j2reed@gmail.com>
2018-09-01 15:00:59 -0400

    Add PORT var; Update package.json
    Added "engine" key with object value to package.json. Stored specific "node" and "npm" versions.

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require("express"); //import using Common.js modules
 const app = express();
+
+// Route Handler for "Get" type
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+//If enviroment variable is defined by host, assign to 'PORT'; else assign 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
