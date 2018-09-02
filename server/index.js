6c6588f1e136e6e394bbaff1d26a7c5eb13f6634
Cody Reed <cody2j2reed@gmail.com>
2018-09-02 16:36:01 -0400

    import mongoose/connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express"); //import using Common.js modules
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
