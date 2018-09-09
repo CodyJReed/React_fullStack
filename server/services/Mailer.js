47f26a1751fc6971e9e4a397ff1b8e5380672b2f
Cody Reed <cody2j2reed@gmail.com>
2018-09-08 20:59:50 -0400

    added services/
    Mailer wired to sendgrid
    /emailTemplates/surveyTemplate HTML created for use with Mailer

---
+++ server/services/Mailer.js
@@ -0,0 +1,57 @@
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email("no-reply@feedback.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+  // Helper Functions created per Sendgrid API
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      // Format email using helper.email the return
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  // Create empty Request and attach object
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON()
+    });
+
+    // Send request to Sendgrid
+    const response = await this.sgApi.API(request);
+    return response;
+  }
+}
+
+module.exports = Mailer;
